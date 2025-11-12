5ba539c1b29c17b63d729f47ae42d0cc5ad8ea2a
Sogba <gametik00@gmail.com>
2025-11-12 20:55:00 +0100

    Work on structure and CMake

--- software/src/main.cpp
+++ software/src/main.cpp
@@ -1,7 +1,57 @@
 #include <iostream>
-#include <vector>
+#include <SFML/Graphics.hpp>
+
+class FPS
+{ 
+public:
+	FPS() : mFrame(0), mFps(0) {}
+	const unsigned int getFPS() const { return mFps; }
+
+private:
+	unsigned int mFrame;
+	unsigned int mFps;
+	sf::Clock mClock;
+
+public:
+	void update()
+	{
+		if(mClock.getElapsedTime().asSeconds() >= 1.f)
+		{
+			mFps = mFrame;
+			mFrame = 0;
+			mClock.restart();
+		}
+
+		++mFrame;
+	}
+};
 
 int main(){
-	std::cout << "Hello World\n";
-  std::vector<int> v1;
+	std::cout << "ROP started\n";
+  FPS fps;
+	sf::ContextSettings context;
+	context.antiAliasingLevel = 8;
+
+	sf::RenderWindow window(sf::VideoMode({800, 600}), "ROP", sf::Style::Default, sf::State::Windowed, context);
+
+	sf::CircleShape triangle(90.f, 3);
+
+	while(window.isOpen()){
+		while (const std::optional event = window.pollEvent()){
+			if (event->is<sf::Event::Closed>())
+					window.close();
+		}
+
+		window.clear(sf::Color::Black);
+
+		window.draw(triangle);
+
+		window.display();
+
+
+		fps.update();
+		std::cout << "FPS:" << fps.getFPS() << "\r";	
+	}
+
+	std::cout << "ROP ended\n";
 }
